03425b210f1eb3d9c22bd6658f08451fc18cf2dc
Johknotter Hill <johknotterhill@gmail.com>
2018-10-30 17:19:03 -0400

    Added the request module and created the single-movie view

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const request = require('request');
 
 const creds = require(`../config/creds`);
-// const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
 const apiBaseUrl = 'http://api.themoviedb.org/3';
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${creds.apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
